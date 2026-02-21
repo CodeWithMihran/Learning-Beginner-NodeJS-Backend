4f3d9c9f9cf47934b42152ae6158d6698309c2a2
CodeWithMihran <sohail.mihran@gmail.com>
2026-02-21 17:02:32 +0530

    Learning NodeJS (File System Codes)

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
 const fs = require('fs');
 
+
+// To make a file with some data or text
+
 // fs.writeFile("first.txt", "This is the data", function(err){
 //     if(err) console.error(err);
 //     else console.log("Done");
 // })
+
+
+// To appemd the file
 
 // fs.appendFile("first.txt", "\nAuthor - Md Mihran Sohail", function(err){
 //     if(err) console.error(err);
 //     else console.log("Done");
 // })
 
-fs.rename("first.txt", "Data.txt", function(err){
-    if(err) console.error(err);
-    else console.log("Done");
+
+// To rename the file
+
+// fs.rename("first.txt", "Data.txt", function(err){
+//     if(err) console.error(err);
+//     else console.log("Done");
+// })
+
+
+// To copy the file
+
+// fs.copyFile("Data.txt", "./copy/Data1.txt", function(err){
+//     if(err) console.error(err.message);
+//     else console.log("Done");
+// })
+
+
+// To Remove the file
+
+// fs.unlink("./copy/Data1.txt", function(err){
+//     if(err) console.error(err.message);
+//     else console.log("Removed");
+// })
+
+
+// To Remove the folder
+
+// fs.rmdir("./copy",{recursive: true}, function(err){
+//     if(err) console.error(err.message);
+//     else console.log("Removed");
+// })
+
+fs.rm("./copy",{recursive: true}, function(err){
+    if(err) console.error(err.message);
+    else console.log("Removed");
 })
