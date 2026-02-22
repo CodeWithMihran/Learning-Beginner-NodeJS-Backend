ab8a5b0269a22db6cf3c971b2030c8e52e090f26
CodeWithMihran <sohail.mihran@gmail.com>
2026-02-23 01:40:19 +0530

    Learning NodeJS (Making the Note Maker Project)

--- Small-Project/index.js
+++ Small-Project/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 // Setting up parsers for Forms
 app.use(express.json());
@@ -14,18 +15,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("index");
+    fs.readdir(`./files`, function(err, files){
+        res.render("index", {files: files});
+    })
+});
+
+app.get("/files/:filename", function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata: filedata});
+    })
+});
+
+app.post("/create", function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    })
 });
 
 
 // Dynamic Routing using colon
-app.get("/profile/:username", function(req, res){
-    res.send(`wlecome, ${req.params.username}`);
-});
+// app.get("/profile/:username", function(req, res){
+//     res.send(`wlecome, ${req.params.username}`);
+// });
 
-app.get("/profile/:username/:age", function(req, res){
-    res.send(`wlecome, ${req.params.username} of Age : ${req.params.age}`);
-});
+// app.get("/profile/:username/:age", function(req, res){
+//     res.send(`Welcome, ${req.params.username} of Age : ${req.params.age}`);
+// });
 
 app.listen(3000, function(){
     console.log("Its running");
